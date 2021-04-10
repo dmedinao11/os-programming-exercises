951317289f7e1ecc2ab8babf8e41e219ce3a9dd9
Daniel Medina <dmedinao@unal.edu.co>
2021-04-09 21:55:44 -0500

    Parte 2 del ejercicio

---
+++ Excercise1/PartII/simple.c
@@ -0,0 +1,24 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
+
+static int simple_init(void)
+{
+       printk(KERN INFO "%lu∖n", GOLDEN RATIO PRIME);
+       return 0;
+}
+
+
+static void simple_exit(void) {
+	unsigned long result = gcd(3300, 24);
+	printk(KERN_INFO "gcd(3300, 24) = %lu∖n", result);
+}
+
+module_init( simple_init );
+module_exit( simple_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
